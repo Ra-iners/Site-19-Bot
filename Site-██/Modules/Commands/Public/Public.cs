067a6784de24131707564d673ccf8a2de1b716e5
rainers <rainersbocs6@gmail.com>
2022-04-12 00:41:25 +0300

    Create command to generate embed for Tickets

--- Site-██/Modules/Commands/Public/Public.cs
+++ Site-██/Modules/Commands/Public/Public.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,46 @@ namespace Site___.Modules.Commands.Public
             }
             
             await ReplyAsync(Response.ToString());
+        }
+
+        [Command("TicketDisplay", RunMode = RunMode.Async)]
+        public async Task TicketDisplay()
+        {
+            EmbedBuilder CTicket = new();
+
+            CTicket.Color = new Color(233, 89, 110);
+            CTicket.Title = ":tickets: Create A Ticket.";
+            CTicket.Description = $"Is someone breaking the rules? or maybe you have a question to ask?\nYou can directly contact staff by selecting your issue below";
+            CTicket.WithFooter("Miss-use of this feature can get you punished!");
+
+            ComponentBuilder cmp = new();
+            cmp.WithSelectMenu(new SelectMenuBuilder()
+            {
+                CustomId = "create_ticket",
+                Placeholder = "Select a Ticket reason",
+                Options = new()
+                {
+                    new()
+                    {
+                        Label = "Rule Breaker",
+                        Value = "Rule Breaker",
+                        Description = $"Someone breaking the rules? Use this to report them."
+                    },
+                    new()
+                    {
+                        Label = "General Question",
+                        Value = "General Question",
+                        Description = $"Have a question that regular members cannot answer? Ask it here."
+                    },
+                    new()
+                    {
+                        Label = "Other",
+                        Value = "Other",
+                        Description = $"Is your reason not listed here? Use this instead."
+                    }
+                }
+            });
+            await Context.Message.ReplyAsync(null, embed: CTicket.Build(), components: cmp.Build());
         }
     }
 }
