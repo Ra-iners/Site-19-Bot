dcda23414930c918b1be79aa3e4e1c3bae7971fc
rainers <rainersbocs6@gmail.com>
2022-04-12 00:14:26 +0300

    Remove non-required dependencies

--- Site-██/Enums/Modstat.cs
+++ Site-██/Enums/Modstat.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Site___.Enums
+﻿namespace Site___.Enums
 {
     public enum Modstat
     {
